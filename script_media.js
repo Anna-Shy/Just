9c5f98cc09a8a6a225383580a5ddaff7388e36f8
Anna-Shy <55240742+Anna-Shy@users.noreply.github.com>
2021-10-04 16:52:08 +0300

    Add files via upload

---
+++ script_media.js
@@ -0,0 +1,56 @@
+import {Media} from './media_constructor.js'
+ 
+ class Book extends Media{
+   constructor(author, title, pages, isCheckedOut, ratings){
+     super(title, isCheckedOut, ratings);
+     this._author = author;
+     this._pages = pages;
+   }
+ 
+   get author(){
+     return this._author;
+   }
+ 
+   get pages(){
+     return this._pages;
+   }
+ }
+ 
+ class CD {
+ 
+ }
+ 
+ class Movie extends Media{
+   constructor(director, title, runTime , isCheckedOut, ratings){
+     super(title, isCheckedOut, ratings);
+     this._director = director;
+     this._runTime = runTime;
+   }
+ 
+    get director(){
+     return this._director;
+   }
+ 
+   get runTime(){
+     return this._runTime;
+   }
+ }
+ 
+const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
+const speed = new Movie('Jan de Bont', 'Speed', 116);
+
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything.isCheckedOut);
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+console.log(historyOfEverything.getAverageRating());
+console.log(historyOfEverything);
+
+speed.toggleCheckOutStatus();
+console.log(speed.isCheckedOut);
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+console.log(speed.getAverageRating());
+console.log(speed);
---
+++ script_school.js
@@ -0,0 +1,72 @@
+class School {
+    constructor(name, level, numberOfStudents){
+        this._name = name;
+        this._level = level;
+        this._numberOfStudents = numberOfStudents;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    get level(){
+        return this._level;
+    }
+
+    get numberOfStudents(){
+        return this._numberOfStudents;
+    }
+
+    set numberOfStudents(value){
+        if(typeof value === Number){
+        this._numberOfStudents = value;
+        }else{
+            console.log('Invalid input: numberOfStudents must be set to a Number.');
+        }
+    }
+
+    quickFacts(){
+        console.log(`${this.name} educates ${this.numberOfStudents} students at the ${this.level} school level.`);
+    }
+
+    static pickSubstituteTeacher(substituteTeachers){
+        let randomTeacher = Math.floor(Math.random() * substituteTeachers.length);
+        return substituteTeachers[randomTeacher];
+    }
+}
+
+class Primary extends School{
+    constructor(name, numberOfStudents, pickupPolicy){
+        super(name, numberOfStudents);
+        this._pickupPolicy = pickupPolicy;
+    }
+
+    get pickupPolicy(){
+        return this._pickupPolicy;
+    }
+}
+
+class Middle {
+
+}
+
+class High extends School{
+    constructor(name, numberOfStudents, sportsTeams){
+        super(name, numberOfStudents);
+        this._sportsTeams = sportsTeams;
+    }
+
+    get sportsTeams(){
+        return this._sportsTeams;
+    }
+}
+
+const lorraineHansbury = new Primary('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+const alSmith = new High('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+
+lorraineHansbury.quickFacts();
+School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+
+console.log(lorraineHansbury);
+console.log(alSmith);
+
