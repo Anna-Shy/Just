6e8ae23caae767e70fbea6e467ed6439e43cbc73
Anna-Shy <shylkina.ao@gmail.com>
2022-05-26 13:08:15 +0300

    style

--- script_media.js
+++ script_media.js
@@ -1,43 +1,45 @@
-import {Media} from './media_constructor.js'
- 
- class Book extends Media{
-   constructor(author, title, pages, isCheckedOut, ratings){
-     super(title, isCheckedOut, ratings);
-     this._author = author;
-     this._pages = pages;
-   }
- 
-   get author(){
-     return this._author;
-   }
- 
-   get pages(){
-     return this._pages;
-   }
- }
- 
- class CD {
- 
- }
- 
- class Movie extends Media{
-   constructor(director, title, runTime , isCheckedOut, ratings){
-     super(title, isCheckedOut, ratings);
-     this._director = director;
-     this._runTime = runTime;
-   }
- 
-    get director(){
-     return this._director;
-   }
- 
-   get runTime(){
-     return this._runTime;
-   }
- }
- 
-const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
-const speed = new Movie('Jan de Bont', 'Speed', 116);
+import { Media } from "./media_constructor.js";
+
+class Book extends Media {
+  constructor(author, title, pages, isCheckedOut, ratings) {
+    super(title, isCheckedOut, ratings);
+    this._author = author;
+    this._pages = pages;
+  }
+
+  get author() {
+    return this._author;
+  }
+
+  get pages() {
+    return this._pages;
+  }
+}
+
+class CD {}
+
+class Movie extends Media {
+  constructor(director, title, runTime, isCheckedOut, ratings) {
+    super(title, isCheckedOut, ratings);
+    this._director = director;
+    this._runTime = runTime;
+  }
+
+  get director() {
+    return this._director;
+  }
+
+  get runTime() {
+    return this._runTime;
+  }
+}
+
+const historyOfEverything = new Book(
+  "Bill Bryson",
+  "A Short History of Nearly Everything",
+  544
+);
+const speed = new Movie("Jan de Bont", "Speed", 116);
 
 historyOfEverything.toggleCheckOutStatus();
 console.log(historyOfEverything.isCheckedOut);
